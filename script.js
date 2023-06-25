a15e2a66fa47560af3def15d2b8835a2740764f7
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-06-25 20:39:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+const listItem=document.getElementById("list");
+
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+touristSpots.sort();
+
+touristSpots.forEach((item)=>{
+	const list=document.createElement("li");
+	list.innerText=item;
+
+	listItem.appendChild(list);
+})
